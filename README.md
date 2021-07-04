fa8f53316cfb8a63b2e37bc0320cef8ae9838646
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2021-07-04 16:00:52 -0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,52 @@
+﻿Lisa just got a new math workbook. A workbook contains exercise problems, grouped into chapters. Lisa believes a problem to be special if its index (within a chapter) is the same as the page number where it's located. The format of Lisa's book is as follows:
+
+There are  chapters in Lisa's workbook, numbered from  to .
+The  chapter has  problems, numbered from  to .
+Each page can hold up to  problems. Only a chapter's last page of exercises may contain fewer than  problems.
+Each new chapter starts on a new page, so a page will never contain problems from more than one chapter.
+The page number indexing starts at .
+Given the details for Lisa's workbook, can you count its number of special problems?
+
+Example
+
+Lisa's workbook contains  problems for chapter , and  problems for chapter . Each page can hold  problems.
+
+The first page will hold  problems for chapter . Problem  is on page , so it is special. Page  contains only Chapter , Problem , so no special problem is on page . Chapter  problems start on page  and there are  problems. Since there is no problem  on page , there is no special problem on that page either. There is  special problem in her workbook.
+
+Note: See the diagram in the Explanation section for more details.
+
+Function Description
+
+Complete the workbook function in the editor below.
+
+workbook has the following parameter(s):
+
+int n: the number of chapters
+int k: the maximum number of problems per page
+int arr[n]: the number of problems in each chapter
+Returns
+- int: the number of special problems in the workbook
+
+Input Format
+
+The first line contains two integers  and , the number of chapters and the maximum number of problems per page.
+The second line contains  space-separated integers  where  denotes the number of problems in the  chapter.
+
+Constraints
+
+Sample Input
+
+STDIN       Function
+-----       --------
+5 3         n = 5, k = 3
+4 2 6 1 10  arr = [4, 2, 6, 1, 10]
+Sample Output
+
+4
+Explanation
+
+The diagram below depicts Lisa's workbook with  chapters and a maximum of  problems per page. Special problems are outlined in red, and page numbers are in yellow squares:
+
+[a relative link](workbook.png)
+
+There are  special problems and thus we print the number  on a new line.
---
+++ Solution.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Workbook_Problem
+{
+    class Result
+    {
+
+        public static int workbook(int n, int k, List<int> arr)
+        {
+            int special_count = 0;
+            int page = 1;
+            int problems_per_page = 0;
+            bool newChapter = false;
+            List<ProblemList> problem_list = new List<ProblemList>();
+
+            // assembles problem_list
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = 0; j < arr.ElementAt(i); j++)
+                {
+                    problem_list.Add(new ProblemList(i + 1, j + 1));
+                }
+            }
+
+            for (int x = 0; x < arr.Count; x++)
+            {
+                newChapter = false;
+                for (int y = 0; y < problem_list.Count; y++)
+                {
+                    if ((problem_list.ElementAt(y).chapter > (x + 1)))
+                    {
+                        problems_per_page = 0;
+                        page++;
+                        newChapter = true;
+                        break;
+                    }
+                    else if ((problem_list.ElementAt(y).chapter == (x + 1))
+                    && (problems_per_page < k))
+                    {
+                        problem_list.ElementAt(y).page = page;
+                        problems_per_page++;
+                    }
+                    else if ((problem_list.ElementAt(y).chapter == (x + 1))
+                    && (problems_per_page >= k))
+                    {
+                        page++;
+                        problems_per_page = 1;
+                        problem_list.ElementAt(y).page = page;
+                    }
+                }
+                if (newChapter)
+                {
+                    continue;
+                }
+            }
+
+            foreach (ProblemList pl in problem_list)
+            {
+                if ((pl.problem == pl.page))
+                {
+                    special_count++;
+                }
+            }
+
+            return special_count;
+        }
+    }
+
+    class ProblemList
+    {
+        public int chapter { get; set; }
+        public int problem { get; set; }
+        public int page { get; set; }
+
+        public ProblemList(int c, int p)
+        {
+            chapter = c;
+            problem = p;
+        }
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+//            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = Result.workbook(n, k, arr);
+            Console.WriteLine(result);
+
+            //            textWriter.WriteLine(result);
+
+            //            textWriter.Flush();
+            //            textWriter.Close();
+        }
+    }
+
+}
